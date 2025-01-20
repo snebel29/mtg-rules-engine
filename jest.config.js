c324ad443bf623c0e1e6c764d24675b006ab52c2
Sven Nebel <nebel.sven@gmail.com>
2025-01-20 18:28:53 +0000

    Create imoport aliases

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  moduleNameMapper: {
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@common/(.*)$': '<rootDir>/src/models/common/$1',
+    '^@game/(.*)$': '<rootDir>/src/models/game/$1',
+  }
 };
